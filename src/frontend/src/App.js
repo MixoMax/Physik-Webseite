d6060f9d2b82a053a52919f3187954404bb24928
Paul Niemann <122018518+ExspandSoftware@users.noreply.github.com>
2024-02-14 11:38:50 +0100

    changes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
