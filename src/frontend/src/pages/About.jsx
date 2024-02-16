edd0d28158aa1844b65098622608bf5bf4f41baa
Paul Niemann <122018518+ExspandSoftware@users.noreply.github.com>
2024-02-16 10:24:46 +0100

    changes

--- src/frontend/src/pages/About.jsx
+++ src/frontend/src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/about.css'
+import './css/about.css'
 
 function About() {
   return (
--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/home.css'
+import './css/home.css'
 
 function Home() {
   return (
