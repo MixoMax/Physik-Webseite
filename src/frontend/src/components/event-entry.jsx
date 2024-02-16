bf678f93d851e12d93f332b22dcd05eebe5d4320
MisterXYCB <81521562+MisterXYCB@users.noreply.github.com>
2024-02-16 10:16:13 +0100

    event-page

--- src/frontend/src/components/event-entry.jsx
+++ src/frontend/src/components/event-entry.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Button } from 'react'
 import './css/event-entry.css'
 
 function EventEntry({ event }) {
@@ -6,7 +6,10 @@ function EventEntry({ event }) {
     <div>
       <div id="event-component-wrapper">
         <img id="event-image" src={event.img ? event.img : "https://m.media-amazon.com/images/I/81Bd9H3HP1L._AC_UF894,1000_QL80_.jpg"} alt="placeholder" />
-        <p>{event.title}</p>
+        <p id="event-title">{event.title}</p>
+        <p id="event-description">{event.description}</p>
+        <p id="event-date">{event.date}</p>
+        <button id="buy-ticket"><img id="ticket-icon" src='https://cdn-icons-png.flaticon.com/512/785/785581.png'/><p1 id="buy-text">Ticket Kaufen</p1></button>
       </div>
     </div>
   )
--- src/frontend/src/pages/Events.jsx
+++ src/frontend/src/pages/Events.jsx
@@ -5,9 +5,9 @@ import EventEntry from '../components/event-entry'
 
 function Events() {
     const events = [
-        { title: "Event 1", date: "01.01.2021", location: "Location 1" },
-        { title: "Event 2", date: "02.02.2022", location: "Location 2" },
-        { title: "Event 3", date: "03.03.2023", location: "Location 3" }
+        { title: "Event 1", date: "01.01.2021", location: "Location 1", description: "This is a Placeholdertex This is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a Placeholdertext" }, 
+        { title: "Event 2", date: "02.02.2022", location: "Location 2", description: "This is a Placeholdertex This is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a Placeholdertext" },
+        { title: "Event 3", date: "03.03.2023", location: "Location 3", description: "This is a Placeholdertex This is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a Placeholdertext" }
     ]
     
 
