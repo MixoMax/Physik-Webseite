277d36bef1adb6ce44de30efb30bbfab951abce6
MixoMax <linushorn@gmx.net>
2024-02-23 10:51:05 +0100

    added slide animation for event page

--- src/frontend/src/pages/Events.jsx
+++ src/frontend/src/pages/Events.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import './css/events.css'
+import { useEffect } from 'react'
 
 import EventEntry from '../components/event-entry'
 
 function Events() {
-    const events = [
-        { title: "Event 1", date: "01.01.2021", location: "Location 1", description: "This is a Placeholder text This is a Placeholder texThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a Placeholdertext" }, 
-        { title: "Event 2", date: "02.02.2022", location: "Location 2", description: "This is a Placeholder text This is a Placeholder texThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a Placeholdertext" },
-        { title: "Event 3", date: "03.03.2023", location: "Location 3", description: "This is a Placeholder text This is a Placeholder text This is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a PlaceholdertexThis is a Placeholdertext" }
-    ]
-    
 
-  return (
-    <div>
-        <p1>Wir sind die besten</p1>
-        <div id="event-list">
-            {events.map((event) => <EventEntry event={event} />)}
+
+    const events = [];
+    for (let i = 0; i < 100; i++) {
+        events.push({ title: "Event " + i, date: "01.01.1970", location: "Location 1", description: "This is a Placeholder text This is a Placeholder tex"});
+    }
+
+    return (
+        <div>
+            <div id="events-wrapper">
+                {events.map((event, index) => (
+                    <EventEntry key={index} event={event} />
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Events
