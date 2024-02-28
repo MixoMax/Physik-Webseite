34ad8fdb143908a68f1571b40ae5af2fa9345918
XuanHa0 <160014090+XuanHa0@users.noreply.github.com>
2024-02-28 12:09:03 +0100

    first commit from me

--- src/frontend/src/pages/Impressum.jsx
+++ src/frontend/src/pages/Impressum.jsx
@@ -3,24 +3,52 @@ import './css/impressum.css';
 
 const Impressum = () => {
     return (
-        <div className="impressum-wrapper">
-            <div className="impressum-card">
-                <h1><strong>Impressum</strong></h1>
-                <h2>Geschäftsdaten</h2>
-                <p><strong>Planetarium Hamburg</strong><br/>Landesbetrieb der Freien und Hansestadt Hamburg (Kulturbeh&ouml;rde)</p>
-                <p>Direktor: Dr. Bj&ouml;rn<br/>VossKaufm&auml;nnischer Gesch&auml;ftsf&uuml;hrer: Thorsten P&uuml;ck</p>
-                <p>Telefon: +49 (40) 428 86 52 -0<br/>Fax: +49 (40) 428 86 52 -99<br/>E-Mail: info(at)planetarium-hamburg.de</p>
-                <br/>
-                <h2>Quellen</h2>
-                <p>Quelle 1</p>
-                <p>Quelle 2</p>
-                <p>Quelle 3</p>
-                <p>Quelle 4</p>
-                <p>Quelle 5</p>
-                <p>Quelle 6</p>
-                <p>Quelle 7</p>
-                <p>Quelle 8</p>
-                <p>Quelle 9</p>
+        <div class="zb-i">
+            <div class="zb-nmg-wrapper row zm-fl">
+                <div class="zb-ce zb-ce-text  ce-headline  zm-section-bottom-md   column ">
+                    <h1 class="h1">Impressum</h1>
+                </div>
+                <div class="zb-ce zb-ce-text  ce-text  zm-section-bottom-md   column ">
+                    <p><strong>Planetarium Hamburg</strong><br/>Landesbetrieb der Freien und Hansestadt Hamburg (Kulturbehörde)<br/>Linnering 1 (Stadtpark)<br/>22299 Hamburg</p>
+                    <p>Direktor: Dr. Björn Voss<br/>Kaufmännischer Geschäftsführer: Thorsten Pück</p>
+                    <p>Telefon: +49 (40) 428 86 52 -0<br/>Fax: +49 (40) 428 86 52 -99<br/>E-Mail: info(at)planetarium-hamburg.de</p>
+                    <p>Umsatzsteuer-Identifikationsnummer/VAT-Number: DE 1185 09 725</p>
+                    <p>Inhaltliche Gesamtverantwortung gemäß § 6 MDStV: Dr. Björn Voss</p>
+                    <p><strong>Informationsangaben zum Ticketverkauf</strong><br/>Außergerichtliche Streitbeilegung Die Europäische Kommission stellt eine Plattform zur außergerichtlichen Online-Streitbeilegung bereit, die Sie unter <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener">https://ec.europa.eu/consumers/odr/</a> finden (Art. 14 Abs. 1 ODR-VO). Der Landesbetrieb Planetarium nimmt an einem Streitbeilegungsverfahren nach dem Verbraucherstreitbeilegungsgesetz vor einer Verbraucherschlichtungsstelle nicht teil (§ 36 VSBG).</p>
+                    <p><strong>Konzeption, Design und Programmierung</strong><br/>THREENET<br/><a href="http://www.threenet.de" target="_blank" rel="noopener">www.threenet.de</a></p>
+                </div>
+                <div class="zb-ce zb-ce-text  ce-text  zm-section-bottom-md   column ">
+                    <h2 class="h2">Urheberrecht</h2>	<p>Das Layout der Homepage, die verwendeten Grafiken, die Tondokumente, die einzelnen Beiträge sowie die Sammlung der Beiträge sind urheberrechtlich geschützt.</p>
+                    <p>Die Beiträge, insbesondere die Beiträge im Bereich "Presse" dürfen durch Medien bei Nennung des Planetarium Hamburg weiter verbreitet werden.</p>
+                    <p>Die Bilder sind urheberrechtlich geschützt. Die Bilder und Grafiken, insbesondere die Bilder im Bereich "Presse" dürfen durch Medien unter Einhaltung der jeweiligen genannten Beschränkungen, bei Nennung des Fotografen oder Urhebers für die Berichterstattung des Planetarium Hamburg verwendet werden.</p>
+                    <p>Die Seiten dürfen nur zum privaten Gebrauch vervielfältigt, Änderungen nicht vorgenommen und Vervielfältigungsstücke ohne Genehmigung nicht verbreitet werden.</p>
+                </div>
+                <div class="zb-ce zb-ce-text  ce-headline    column ">
+                    <h2 class="h2">Bildrechte</h2>
+                </div>  
+                <div class="zb-ce zb-ce-text  ce-copyright-list    column ">
+                    <ul class="copy__category-list">
+                        <li class="copy__category-list__item">
+                        <h3 class="h3 copy__category-list__hl">Andere Quellen:</h3>
+                            <ul class="copy__owner-list">
+                                <li class="copy__owner-list__item">
+                                <h4 class="h4 copy__owner-list__hl">Audivisual Imagineering AVI</h4>
+                                    <ul class="copy__file-list list-unstyled list-inline">
+                                        <li class="copy__file-list__item">
+                                            <a class="copy__file-list__link" href="files/usr/module/produktionen/kleiner-stern-im-grossen-all/kleiner-stern-im-grossen-all-galerie-2.jpg" title="files/usr/module/produktionen/kleiner-stern-im-grossen-all/kleiner-stern-im-grossen-all-galerie-2.jpg" target="_blank">
+                                            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                    </ul>   
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="zb-ce zb-ce-text  ce-text  zm-section-top-md   column ">
+                        <h2 class="h2">Haftungsausschluss</h2>	
+                        <p>Die auf dieser Internetseite veröffentlichten Inhalte sind sorgfältig recherchiert. Dennoch kann das Planetarium Hamburg keine Gewähr für die Aktualität, Richtigkeit und Vollständigkeit der bereitgestellten Informationen übernehmen. In keinem Fall wird für Schäden, die sich aus der Verwendung der abgerufenen Informationen ergeben, eine Haftung übernommen.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
