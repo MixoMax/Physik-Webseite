d8ebba6425e0a301810d06669d8fd0e68ffa85c2
Linus Horn <linushorn@gmx.net>
2024-03-03 17:03:55 +0100

    added github webhook

--- src/backend/main.py
+++ src/backend/main.py
@@ -173,6 +173,20 @@ async def filter_events(request: Request) -> JSONResponse:
 
 
 
+@app.post("/github_webhook")
+async def github_webhook(request: Request) -> JSONResponse:
+    data = await request.json()
+    
+    commands = [
+        "git fetch && git pull",
+        "cd ~/Physik-Webseite/src/frontend"
+        "npm run build"
+    ]
+
+    for command in commands:
+        os.system(command)
+
+    return JSONResponse({"message": "Received webhook"})
 
 
 
